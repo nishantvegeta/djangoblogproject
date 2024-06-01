79c909263321bf0497954354508c2be58915a0a4
nishantvegeta <nishantkumpakha03@gmail.com>
2024-06-01 17:52:45 +0545

    new commit

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'core',
-        'title' : 'post 1',
-        'content' : 'first post content',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author': 'nish',
-        'title' : 'post 2',
-        'content' : 'second post content',
-        'date_posted' : 'August 28, 2018'
-    },
-]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
